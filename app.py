4668752d876d7d76b3702a3ffea3a846b2648a3e
jackhzt <32922254+jackhzt@users.noreply.github.com>
2019-10-22 01:21:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,'12345')
+    line_bot_api.reply_message(event.reply_token,message)
 
 #主程式
 import os
